a6e3720e4d3601fa429b785fbddee4a8de4df178
Kyle Gach <kyle.gach@gmail.com>
2020-06-23 16:58:40 -0600

    Use `nvm` (via `zsh-nvm`) to manage node
    - Install latest `node` & `npm`

--- node/install.sh
+++ node/install.sh
@@ -1,7 +1,29 @@
-if test ! $(which spoof)
+#!/bin/sh
+
+echo "  Checking for zsh-nvm..."
+if [[ -a ~/.zsh-nvm ]]
 then
-  if test $(which npm)
-  then
-    sudo npm install spoof -g
-  fi
+  echo "  Found zsh-nvm, upgrading..."
+  nvm upgrade
+else
+  echo "  Installing zsh-nvm..."
+  git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh-nvm 
 fi
+
+# Load the zsh-nvm plugin
+export NVM_AUTO_USE=true
+export NVM_COMPLETION=true
+export NVM_LAZY_LOAD=true
+source ~/.zsh-nvm/zsh-nvm.plugin.zsh
+
+echo "  Installing node..."
+nvm install node
+nvm use node
+
+echo "  Installing/upgrading global npm packages..."
+GLOBAL_NPM_PACKAGES=(
+  npm
+);
+npm install -g ${GLOBAL_NPM_PACKAGES[@]}`;
+
+exit 0
