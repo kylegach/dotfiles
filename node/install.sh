36fd9f3e4d2bd3e081439c8243b28d0e954df077
Kyle Gach <kyle.gach@gmail.com>
2020-06-24 17:04:30 -0600

    Install lts node instead of latest
    - Define in .nvmrc

--- node/install.sh
+++ node/install.sh
@@ -1,24 +1,23 @@
 #!/bin/zsh
 
-# We reload the shell a couple times to make `nvm` available
+# We reload the shell to make `nvm` available, either before upgrading [1]
+# or after installing [2]
 
 echo "  Checking for zsh-nvm..."
 DIR=~/.zsh-nvm
 if [[ -a $DIR ]]
 then
   echo "  Found zsh-nvm, upgrading..."
-  source ~/.zshrc
+  source ~/.zshrc # [1]
   nvm upgrade
 else
   echo "  Installing zsh-nvm..."
   git clone https://github.com/lukechilds/zsh-nvm.git $DIR
+  source ~/.zshrc # [2]
 fi
 
-source ~/.zshrc
-
 echo "  Installing node..."
-nvm install node
-nvm use node
+nvm install
 
 echo "  Installing/upgrading global npm packages..."
 GLOBAL_NPM_PACKAGES=(
