07fa0cd1ca6ccd86fd97c3391c79bb70d599c7f9
Kyle Gach <kyle.gach@gmail.com>
2020-06-24 18:53:17 -0600

    Add spaceship-theme prompt
    - Install/upgrade and initialize spaceship-prompt

---
+++ zsh/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "  Checking for spaceship-prompt..."
+DIR=$HOME/.spaceship-prompt
+if [[ -a $DIR ]]
+  then
+    echo "  Found spaceship-prompt; upgrading..."
+    cd $DIR
+    git pull --rebase origin
+    cd -
+  else 
+    echo "  Installing spaceship-prompt..."
+    DIR_IN_FPATH="/usr/local/share/zsh/site-functions"
+    git clone https://github.com/denysdovhan/spaceship-prompt.git $DIR
+    ln -sf "$DIR/spaceship.zsh" "$DIR_IN_FPATH/prompt_spaceship_setup"
+fi
