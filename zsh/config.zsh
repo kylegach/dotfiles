faa6034aea1817a51dd944db8cad6d879bd855ea
Kyle Gach <kyle.gach@gmail.com>
2020-06-24 18:56:28 -0600

    Customize aliases, functions, shell, and env
    - Add aliases
    - Add functions
    - Change projects directory to `~/projects`
    - Add .hushlogin

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -25,6 +25,7 @@ setopt IGNORE_EOF
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_IGNORE_SPACE # omit commands that begin with a space from history
 setopt HIST_REDUCE_BLANKS
 
 # don't expand aliases _before_ completion has finished
