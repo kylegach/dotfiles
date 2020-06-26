5d272ad7943704ac1d52dba026a403ca056cdc59
Kyle Gach <kyle.gach@gmail.com>
2020-06-25 21:12:33 -0600

    Source nvm instead of .zshrc in node/install
    - Was only sourcing .zshrc to access nvm
        - But sourcing before the shell has reloaded the first time (i.e. while running script/bootstrap for the first time) caused errors
        - Sourcing nvm directly avoids the issue
    - Also flipped the order of the "install or upgrade" conditional, to better match the order of actions in practice
        - And propagated that pattern throughout

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,15 +2,15 @@
 
 echo "  Checking for spaceship-prompt..."
 DIR=$HOME/.spaceship-prompt
-if [[ -a $DIR ]]
+if ! [[ -a $DIR ]]
   then
+    echo "  Installing spaceship-prompt..."
+    DIR_IN_FPATH="/usr/local/share/zsh/site-functions"
+    git clone https://github.com/denysdovhan/spaceship-prompt.git $DIR
+    ln -sf "$DIR/spaceship.zsh" "$DIR_IN_FPATH/prompt_spaceship_setup"    
+  else 
     echo "  Found spaceship-prompt; upgrading..."
     cd $DIR
     git pull --rebase origin
     cd -
-  else 
-    echo "  Installing spaceship-prompt..."
-    DIR_IN_FPATH="/usr/local/share/zsh/site-functions"
-    git clone https://github.com/denysdovhan/spaceship-prompt.git $DIR
-    ln -sf "$DIR/spaceship.zsh" "$DIR_IN_FPATH/prompt_spaceship_setup"
 fi
