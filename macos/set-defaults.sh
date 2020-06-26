a0145e6ff5fa059f60a94a28b467eb05ee87de45
Kyle Gach <kyle.gach@gmail.com>
2020-06-25 21:35:49 -0600

    Move "computer name" to bootstrap; avoid prompt
    - It's more of a one-time thing
        - set-defaults runs within dot, which can and should be run whenever
    - The commands don't work without quoting the final argument

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -22,16 +22,6 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # General UI/UX                                                               #
 ###############################################################################
 
-# Set computer name (as done via System Preferences → Sharing)
-# Define $COMPUTER_NAME in ~/.localrc
-if [ -z ${COMPUTER_NAME+x} ]
-  then
-    sudo scutil --set ComputerName $COMPUTER_NAME
-    sudo scutil --set HostName $COMPUTER_NAME
-    sudo scutil --set LocalHostName $COMPUTER_NAME
-    sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
-fi
-
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
 
